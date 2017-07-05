4e510d0f1ff479384f2d6bf72df4b7182497d162
Kishan Gajera <me@kgajera.com>
2017-07-05 19:43:05 -0400

    Update to not output empty markup

--- readme.txt
+++ readme.txt
@@ -1,10 +1,31 @@
-=== WP Link Preview Plugin ===
+=== WP Link Preview ===
 Contributors: kgajera
-Tags: link preview, facebook link preview, teaser, excerpt
+Tags: link preview, facebook link preview, link teaser, link excerpt, share link, url preview, url teaser, url excerpt, share url
+
 Requires at least: 4.7
-Tested up to: 4.7.2
+Tested up to: 4.8
+Stable tag: 1.1
+
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
+Display a preview for a URL similar to sharing a link on Facebook.
+
 == Description ==
-This plugin will add a button to your post/page editor which can be used to generate a link preview for the given URL. The link preview consists of a title, description and image.
+This plugin will add a button to your post/page editor which can be used to generate and embed a link preview for a given URL. The link preview consists of a title, description and image which will result in a similar look as sharing a link on Facebook.
+
+== Installation ==
+1. Uncompress the download package
+1. Upload the folder to the `/wp-content/plugins/` directory.
+1. Activate the plugin through the 'Plugins' menu in WordPress.
+
+== Screenshots ==
+
+1. screenshot-1.png
+2. screenshot-2.png
+3. screenshot-3.png
+
+== Changelog ==
+
+= 1.1 =
+* Remove output of extra HTML when any or all meta data does not exist
--- wplinkpreview.php
+++ wplinkpreview.php
@@ -1,8 +1,7 @@
 <?php
 /**
  * Plugin Name: WP Link Preview
- * Plugin URI: http://kgajera.com
- * Version: 1.0
+ * Version: 1.1
  * Author: Kishan Gajera
  * Author URI: http://www.kgajera.com
  * Description: Turn a URL into a Facebook like link preview
@@ -13,9 +12,15 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 class WPLinkPreview {
 
+    private $url = '';
+
+    private $document;
+
+    private $meta = array();
+
     function __construct() {
         if ( is_admin() ) {
-            add_action( 'init', array( $this, 'init' ) );
+            add_action( 'init', array( $this, 'init_tinymce' ) );
         }
 
         // Enqueue default styles for the link preview HTML
@@ -23,9 +28,50 @@ class WPLinkPreview {
     }
 
     /**
+    * Register plugin front-end stylesheet
+    */
+    function enqueue_scripts() {
+        wp_register_style( 'wplinkpreview-style', plugins_url( '/wplinkpreview.css', __FILE__ ), array(), '20120208', 'all' );
+        
+        wp_enqueue_style( 'wplinkpreview-style' );
+    }
+
+    /**
+    * AJAX action to fetch and output the link preview content
+    */
+    function fetch_wplinkpreview() {
+        $url = esc_url( $_GET['url'] );
+
+        // Validate to ensure we have a URL
+        if ( filter_var( $url, FILTER_VALIDATE_URL ) !== false ) {
+            // Ensure we have a scheme
+            $this->url = preg_replace( '/^(?!https?:\/\/)/', 'http://', $url );
+
+            // Fetch and parse the document at the given URL
+            $this->setup_link_preview();
+
+            if ( ! empty( $this->document ) ) {
+                ?>
+                <div class="<?php $this->the_class(); ?>">
+                    <?php 
+                    $this->the_image();
+                    $this->the_title();
+                    $this->the_description();
+                    $this->the_source();
+                    ?>
+                </div>
+                <br />
+                <?php
+            }
+        }
+
+        wp_die();
+    }
+
+    /**
     * Initialization to add the TinyMCE plugin
     */
-    function init() {
+    function init_tinymce() {
         // Check if the logged in user can edit posts before registering the TinyMCE plugin
         if ( ! current_user_can( 'edit_posts' ) && ! current_user_can( 'edit_pages' ) ) {
             return;
@@ -45,103 +91,18 @@ class WPLinkPreview {
     }
 
     /**
-    * Register plugin front-end stylesheet
-    */
-    function enqueue_scripts() {
-        wp_register_style( 'wplinkpreview-style', plugins_url( '/wplinkpreview.css', __FILE__ ), array(), '20120208', 'all' );
-        
-        wp_enqueue_style( 'wplinkpreview-style' );
-    }
-
-    /**
-    * AJAX action to fetch and output the link preview content
-    */
-    function fetch_wplinkpreview() {
-        $url = esc_url( $_GET['url'] );
-
-        // Validate to ensure we have a URL
-        if ( filter_var( $url, FILTER_VALIDATE_URL ) === false ) {
-            wp_die();
-        }
-
-        // Ensure we have a scheme
-        $url = preg_replace( '/^(?!https?:\/\/)/', 'http://', $url );
-        $parsed_url = parse_url( $url );
-
-        // Make HTTP request and parse the response body
-        $document = $this->fetch_document( $url );
-        $meta = $this->get_document_meta( $document );
-        $title = $this->get_document_title( $meta, $document );
-        $description = $this->get_document_description( $meta, $document );
-        $image_url = $this->get_document_image( $meta, $document );
-        $favicon = $this->get_document_favicon( $document );
-        ?>
-        <div class="wplinkpreview">
-            <?php if ( ! empty( $image_url ) ) { ?>
-                <div class="wplinkpreview-image">
-                    <a href="<?php echo esc_url ( $url ); ?>" target="_blank">
-                        <img src="<?php echo esc_url( $image_url ); ?>" alt="<?php echo esc_attr( $title ); ?>" />
-                    </a>
-                </div>
-            <?php } ?>
-            <div class="wplinkpreview-title">
-                <a href="<?php echo esc_url( $url ); ?>" target="_blank">
-                    <?php echo esc_html( $title ); ?>
-                </a>
-            </div>
-            <div class="wplinkpreview-description">
-                <?php echo esc_html( $description ); ?>
-            </div>
-            <div class="wplinkpreview-source">
-                <?php if ( ! empty( $favicon ) ) { ?>
-                    <img src="<?php echo esc_url( $favicon ); ?>" width="16" height="16" />
-                <?php } ?>
-                <a href="<?php echo esc_url( $url ); ?>" target="_blank">
-                    <?php echo esc_html( $parsed_url["host"] ); ?>
-                </a>
-            </div>
-        </div>
-        <br />
-        <?php
-
-        wp_die();
-    }
-
-    /**
-    * Make HTTP GET request for the inputted URL
-    *
-    * @param string $url
-    * @return a DOMDocument containing the body of the response returned for the given url 
-    */
-    function fetch_document( $url ) {
-        $document = new DOMDocument();
-
-        // Use WordPress HTTP API to make GET request for the given URL
-        $response = wp_remote_get( $url, array( 'timeout' => 120 ) );
-
-        // Parse document
-        if( is_array( $response ) ) {
-            @$document->loadHTML( wp_remote_retrieve_body( $response ) );
-        }
-
-        return $document;
-    }
-
-    /**
     * Return the description of the document. The following tags will be
     * used to find a description until one is found:
     *  1) open graph description meta tag
     *  2) description meta tag
     *
-    * @param array $meta Parsed meta tags stored as key values
-    * @param DOMDocument $document
     * @return string A description for the document
     */
-    function get_document_description( $meta, $document ) {
-        $description = $meta['og:description'];
+    function get_document_description() {
+        $description = $this->meta['og:description'];
     
         if ( empty( $description ) ) {
-            $description = $meta['description'];
+            $description = $this->meta['description'];
         }
 
         return $description;
@@ -152,25 +113,20 @@ class WPLinkPreview {
     * used to find an image until one is found:
     *  1) open graph image meta tag
     *
-    * @param array $meta Parsed meta tags stored as key values
-    * @param DOMDocument $document
     * @return string A image URL
     */
-    function get_document_image( $meta, $document ) {
-        $image = $meta['og:image'];
-    
-        return $image;
+    function get_document_image() {   
+        return $this->meta['og:image'];
     }
 
     /**
     * Return the favicon for the document
     *
-    * @param DOMDocument $document
     * @return string The favicon image URL
     */
-    function get_document_favicon( $document ) {
+    function get_document_favicon() {
         $favicon = '';
-        $links = $document->getElementsByTagName( 'link' );
+        $links = $this->document->getElementsByTagName( 'link' );
 
         for( $i = 0; $i < $links->length; $i++ ) {
             $link = $links->item($i);
@@ -185,55 +141,27 @@ class WPLinkPreview {
     }
 
     /**
-    * Parse all meta tags into a key value array
-    *
-    * @param DOMDocument $document
-    * @return array Array of all meta tags in document stored as a key value pair
-    */
-    function get_document_meta( $document ) {
-        $meta_tags = array();
-
-        $metas = $document->getElementsByTagName('meta');
-        for ($i = 0; $i < $metas->length; $i++) {
-            $meta = $metas->item( $i );
-            $name = $meta->getAttribute( 'name' );
-
-            if ( empty( $name ) ) {
-                $name = $meta->getAttribute( 'property' );
-            }
-
-            if ( ! empty( $name ) ) {
-                $meta_tags[$name] = $meta->getAttribute( 'content' );
-            }
-        }
-
-        return $meta_tags;
-    }
-    
-    /**
     * Return a title for the document. The following tags will be
     * used to find a title until one is found:
     *  1) open graph title meta tag
     *  2) the title tag
     *  3) the first h1 tag
     *
-    * @param array $meta Parsed meta tags stored as key values
-    * @param DOMDocument $document
     * @return string A title
     */
-    function get_document_title( $meta, $document ) {
+    function get_document_title() {
         // 1) Use the open graph title
-        $title = $meta['og:title'];
+        $title = $this->meta['og:title'];
 
         // 2) Use the the document title tag
         if ( empty( $title ) ) {
-            $nodes = $document->getElementsByTagName( 'title' );
+            $nodes = $this->document->getElementsByTagName( 'title' );
             $title = $nodes->item(0)->nodeValue;
         }
 
         // 3) Use first h1 tag
         if ( empty( $title ) ) {
-            $nodes = $document->getElementsByTagName( 'h1' );
+            $nodes = $this->document->getElementsByTagName( 'h1' );
             $title = $nodes->item(0)->nodeValue;
         }
 
@@ -261,6 +189,107 @@ class WPLinkPreview {
     function mce_buttons( $buttons ) {
         array_push( $buttons, '|', 'wplinkpreview_plugin' );
         return $buttons;
+    }
+
+    /**
+    * Parse all meta tags into a key value array
+    *
+    */
+    function parse_document_meta() {
+        $meta_tags = $this->document->getElementsByTagName('meta');
+
+        for ($i = 0; $i < $meta_tags->length; $i++) {
+            $meta = $meta_tags->item( $i );
+            $name = $meta->getAttribute( 'name' );
+
+            if ( empty( $name ) ) {
+                $name = $meta->getAttribute( 'property' );
+            }
+
+            if ( ! empty( $name ) ) {
+                $this->meta[$name] = $meta->getAttribute( 'content' );
+            }
+        }
+    }
+
+    /**
+    * Make HTTP GET request for the inputted URL
+    *
+    */
+    function setup_link_preview() {
+        $this->document = null;
+        $this->meta = array();
+
+        if ( ! empty( $this->url ) ) {
+            // Use WordPress HTTP API to make GET request for the given URL
+            $response = wp_remote_get( $this->url, array( 'timeout' => 120 ) );
+
+            // Load and parse document
+            if( is_array( $response ) ) {
+                $this->document = new DOMDocument();
+                @$this->document->loadHTML( wp_remote_retrieve_body( $response ) );
+                $this->parse_document_meta();
+            }
+        }
+    }
+
+    function the_class( $suffix = '' ) {
+        echo esc_attr( 'wplinkpreview' . ( empty( $suffix ) ? '' : '-' . $suffix ) );
+    }
+
+    function the_description() {
+        $description = $this->get_document_description();
+        
+        if ( ! empty( $description ) ) {
+            ?>
+            <div class="<?php $this->the_class( 'description' ); ?>">
+                <?php echo esc_html( $description ); ?>
+            </div>
+            <?php
+        }
+    }
+
+    function the_image() {
+        $image_url = $this->get_document_image();
+
+        if ( ! empty( $image_url ) ) {
+            ?>
+            <div class="<?php $this->the_class( 'image' ); ?>">
+                <a href="<?php echo esc_url ( $this->url ); ?>" target="_blank">
+                    <img src="<?php echo esc_url( $image_url ); ?>" alt="<?php echo esc_attr( $this->get_document_title() ); ?>" />
+                </a>
+            </div>
+            <?php
+        }
+    }
+
+    function the_source() {
+        $favicon = $this->get_document_favicon();
+        $parsed_url = parse_url( $this->url );
+        ?>
+        <div class="<?php $this->the_class( 'source' ); ?>">
+            <?php if ( ! empty( $favicon ) ) { ?>
+                <img src="<?php echo esc_url( $favicon ); ?>" width="16" height="16" />
+            <?php } ?>
+            <a href="<?php echo esc_url( $this->url ); ?>" target="_blank">
+                <?php echo esc_html( $parsed_url["host"] ); ?>
+            </a>
+        </div>
+        <?php
+    }
+
+    function the_title() {
+        $title = $this->get_document_title();
+
+        if ( ! empty( $title ) ) {
+            ?>
+            <div class="<?php $this->the_class( 'title' ); ?>">
+                <a href="<?php echo esc_url( $this->url ); ?>" target="_blank">
+                    <?php echo esc_html( $title ); ?>
+                </a>
+            </div>
+            <?php
+        }
     }
 
 }
